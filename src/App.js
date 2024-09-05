0edb5b15730c06d8b8f757101a06215e62d94d03
osrr <ossaro02@gmail.com>
2024-09-06 01:15:58 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  // TODO ANAS in this file change the in between brackets after return to
+  /*
+        <div>
+            <h1>(write anything but Hello World)</h1>
+            <img src="picsum.photos/600/600" alt="photo"/>
+        </div>
+    */
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
